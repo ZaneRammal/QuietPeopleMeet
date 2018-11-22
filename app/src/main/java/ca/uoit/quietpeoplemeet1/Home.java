68e61fdff498057f661fcb4d47a4c34ddf007009
zanerammalUOIT <zane.rammal@uoit.net>
2018-11-22 12:10:52 -0500

    Initial commit

---
+++ app/src/main/java/ca/uoit/quietpeoplemeet1/Home.java
@@ -0,0 +1,18 @@
+package ca.uoit.quietpeoplemeet1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+
+
+
+    }
+}
