721578d18e479b0942d4fe146925905849316bed
zanerammalUOIT <zane.rammal@uoit.net>
2018-11-23 13:10:12 -0500

    Created navigation bars and menus. The functionality of these menus is yet to be implemented.

--- app/src/main/java/ca/uoit/quietpeoplemeet1/Test.java
+++ app/src/main/java/ca/uoit/quietpeoplemeet1/Test.java
@@ -3,16 +3,11 @@ package ca.uoit.quietpeoplemeet1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Home extends AppCompatActivity {
+public class Test extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-
-
-
-
-
+        setContentView(R.layout.activity_test);
     }
 }
